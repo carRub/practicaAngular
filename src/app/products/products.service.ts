c3c8c9e0ce302b9e6fca8300dad727ebad821053
Carlos Rubio <carub96@gmail.com>
2020-04-12 23:53:44 -0500

    Getting the service done

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Product, Especificacion } from './Product';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  // array with all the existing products
+  products: Product[];
+  // array with all the monitor products ids
+  monitoredProducts: number[];
+  // To use in the contructor
+  dummySpecification: Especificacion[];
 
-  constructor() { }
+  // Subjects
+  productsSubject = new Subject<Product[]>();
+  monitoredProductsSubject = new Subject<number[]>();
+
+
+  constructor() {
+    this.products = [new Product(1, 'smartTV', 'Samsung', 'Samsung smartTv', 15000, 13, this.dummySpecification),
+                    new Product(1, 'smartPhone', 'Huawei', 'Huawei smartphone', 5500, 10, this.dummySpecification),
+                    new Product(1, 'smartWathc', 'HTC', 'HTC smartwatch', 3500, 8, this.dummySpecification)];
+  }
+
+  getProducts(): Product[] {
+    return this.products.slice();
+  }
+
+  // add
+
+  // delete
+
+  // update
+
 }
