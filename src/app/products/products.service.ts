6db527b750ea5c287ffa0a94267f725240f0b6f8
Carlos Rubio <carub96@gmail.com>
2020-04-13 18:02:29 -0500

    Product service has know the add, delete and get functionalities

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -8,14 +8,14 @@ import { Subject } from 'rxjs';
 export class ProductsService {
   // array with all the existing products
   products: Product[];
-  // array with all the monitor products ids
-  monitoredProducts: number[];
+  // set with all the monitor products ids
+  monitoredProducts = new Set();
   // To use in the contructor
   dummySpecification: Especificacion[];
 
   // Subjects
   productsSubject = new Subject<Product[]>();
-  monitoredProductsSubject = new Subject<number[]>();
+  monitoredProductsSubject = new Subject<Set<any>>();
 
 
   constructor() {
@@ -24,14 +24,47 @@ export class ProductsService {
                     new Product(1, 'smartWathc', 'HTC', 'HTC smartwatch', 3500, 8, this.dummySpecification)];
   }
 
+  // get
   getProducts(): Product[] {
     return this.products.slice();
   }
 
+  getMonitoredProducts(): Set<any> {
+    return new Set(this.monitoredProducts);
+  }
+
   // add
+  addProduct(product) {
+    console.log('adding a new product');
+    this.products.push(product);
+    this.productsSubject.next(this.getProducts());
+  }
+
+  addMonitoredProduct(id) {
+    console.log('adding a monitored product');
+    this.monitoredProducts.add(id);
+    this.monitoredProductsSubject.next(this.getMonitoredProducts());
+  }
 
   // delete
+  deleteProduct(id) {
+    console.log('deleting');
 
-  // update
+    // checking if we need to delete something from the monitored products
+    if (this.monitoredProducts.has(id)) {
+      this.deleteMonitoredProduct(id);
+    }
+
+    this.products.splice(this.products.findIndex(o => o.uid === id), 1);
+    this.productsSubject.next(this.getProducts());
+  }
+
+  deleteMonitoredProduct(id) {
+    console.log('deleting monitored product');
+    this.monitoredProducts.delete(id);
+    this.monitoredProductsSubject.next(this.getMonitoredProducts());
+  }
+
+  // update TODO: check if the product changes on the monitored
 
 }
